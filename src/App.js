f259b12a2e048dee7d511bf008d7c23cc093564a
Nahidhassan1 <nahidlikhon22@gmail.com>
2022-11-08 00:45:52 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './routers/Routs/Routes';
 
 function App() {
   return (
     <div className="App">
+      <RouterProvider router={router}>
+      </RouterProvider>
     </div>
   );
 }
