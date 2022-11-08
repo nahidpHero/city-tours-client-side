cd304d4b929829de1913c478f421b146c9d900ba
Nahidhassan1 <nahidlikhon22@gmail.com>
2022-11-09 01:59:35 +0600

    photoviewer added

--- src/pages/Home/products/ProductCard.js
+++ src/pages/Home/products/ProductCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 
 const ProductCard = ({product}) => {
@@ -8,7 +10,12 @@ const ProductCard = ({product}) => {
     return (
         <div>
             <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src={img} alt="Shoes" /></figure>
+            <PhotoProvider>
+      <PhotoView src={img}>
+        <img src={img} alt="" />
+      </PhotoView>
+    </PhotoProvider>
+  {/* <figure><img src= alt="Shoes" /></figure> */}
   <div className="card-body">
     <h2 className="card-title">{name}</h2>
     <p>{details?.slice(0,100)}...</p>
